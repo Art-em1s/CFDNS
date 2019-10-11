bd57188a92345528f41005b04c8d39e25cdd173f
Artemis <Artemis@w1z0.xyz>
2019-10-11 20:32:11 +0100

    v1

---
+++ update.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import logging
+logging.basicConfig(filename='script.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+
+IP_API = 'https://api.ipify.org?format=json'
+CF_API_KEY = '000'
+CF_EMAIL = 'example@example.com'
+
+headers = {
+    'X-Auth-Email': CF_EMAIL,
+    'X-Auth-Key': CF_API_KEY,
+    'Content-Type': "application/json",
+    }
+
+resp = requests.get(IP_API).json()
+ip = resp['ip']
+
+response = requests.get("https://api.cloudflare.com/client/v4/zones", headers=headers, params={"status":"active","page":"1","per_page":"20","order":"status","direction":"desc","match":"all"}).json() #get all zones
+
+for item in response['result']:
+    r = requests.get("https://api.cloudflare.com/client/v4/zones/{}/dns_records?type=A".format(item['id']), headers=headers).json() #get record ids for all A records
+    if r['success'] == True: 
+        for record in r['result']:
+            if record['content'] == ip: #don't update domains that match the current ip
+                logging.info("Record '{}' skipped, IP matches".format(record['name']))
+                continue
+            else: #update any that don't match
+                resp = requests.put(
+                    'https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}'.format(item['id'], record['id']),
+                    json={
+                        'type': 'A',
+                        'name': record['name'],
+                        'content': ip,
+                        'proxied': True #ensure domains are proxied
+                    },
+                    headers=headers)
+                assert resp.status_code == 200
+                logging.info("Record '{}' updated to {}".format(record['name'], ip))
+    else:
+        logging.info(r)
